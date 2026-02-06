f2ff11316dc8bd166e6a02b478c91670d65d7e98
Md. Rafi <252-35-120@diu.edu.bd>
2026-02-06 14:42:48 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const countLabel = document.getElementById("counter");
+const incrementBtn = document.getElementById("incrementBtn");
+
+let count = 0;
+
+incrementBtn.addEventListener("click", () => {
+  count += 1;
+  countLabel.textContent = count;
+});
